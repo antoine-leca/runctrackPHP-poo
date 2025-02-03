abe1227cec8d66ddb0c0475409273e7b63a9a92d
Antoine <antoine.leca@laplateforme.io>
2025-02-03 12:50:38 +0100

    ajout du fichier job01 corrigé

--- job01.php
+++ job01.php
@@ -1,11 +1,18 @@
 <?php 
     class Operation {
-        public $nombre1 = 38;
-        public $nombre2 = 24;
+        public $nombre1;
+        public $nombre2;
+
+        public function __construct($nombre1 = 38, $nombre2 = 24) {
+            $this->nombre1 = $nombre1;
+            $this->nombre2 = $nombre2;
+        }
+
         public function afficherNombres() {
             echo $this->nombre1 . " " . $this->nombre2;
         }
     }
+
     $operation = new Operation();
     $operation->afficherNombres();
 ?>
