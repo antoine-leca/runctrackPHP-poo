f6e6af6e8a4cbc4ccc5fbc30e1653f2a93717abf
Antoine <antoine.leca@laplateforme.io>
2025-02-05 18:16:48 +0100

    ajout du fichier job02 fini

---
+++ jour03/job02.php
@@ -0,0 +1,58 @@
+<?php
+    class Personne {
+        public $age = 14;
+
+        public function __construct($age = 14) {
+            $this->age = $age;
+        }
+
+        public function afficherAge() {
+            echo "Âge : " . $this->age . "<br>";
+        }
+
+        public function bonjour() {
+            echo "Hello <br>";
+        }
+
+        public function modifierAge($age) {
+            $this->age = $age;
+        }
+    }
+
+    class Eleve extends Personne {
+        public function allerEnCours() {
+            echo "Je vais en cours <br>";
+        }
+
+        public function afficherAge() {
+            echo "J'ai " . $this->age . " ans <br>";
+        }
+    }
+
+    class Professeur extends Personne {
+        private $matiereEnseignee;
+
+        public function __construct($age, $matiereEnseignee) {
+            parent::__construct($age);
+            $this->matiereEnseignee = $matiereEnseignee;
+        }
+
+        public function enseigner() {
+            echo "Le cours d'" . $this->matiereEnseignee . " va commencer <br>";
+        }
+    }
+
+    $personne = new Personne();
+    $stu = new Eleve();
+    $prof = new Professeur(40, "Anglais");
+
+    $stu->afficherAge();
+
+    $stu->modifierAge(15);
+    $stu->allerEnCours();
+    $stu->bonjour();
+
+    $prof->afficherAge();
+    $prof->bonjour();
+    $prof->enseigner();
+?>
